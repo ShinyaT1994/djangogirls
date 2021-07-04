1d47bd998ff598742030196ccf2f84d42421e649
shinya.takaramoto <rm0063vpedc15@gmail.com>
2021-07-04 20:17:08 +0900

    Create base template and post detail template

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,5 +1,5 @@
 # DjangoのImport
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 
 # モデルのImport
@@ -9,3 +9,6 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
